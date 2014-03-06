4a70268e640c9375e5039d5bdb2ad8c7ed090f18
Anna Twardak <Anna.Twardak@ifj.edu.pl>
2014-03-06 22:01:11 +0100

    Remove hardcoded passwords

--- DotBase/LogowanieForm.Designer.cs
+++ DotBase/LogowanieForm.Designer.cs
@@ -104,7 +104,6 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(115, 20);
             this.textBox2.TabIndex = 6;
-            this.textBox2.Text = "npp126p";
             this.textBox2.UseSystemPasswordChar = true;
             // 
             // label1
@@ -140,7 +139,6 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(115, 20);
             this.textBox3.TabIndex = 10;
-            this.textBox3.Text = "doniczka";
             this.textBox3.UseSystemPasswordChar = true;
             // 
             // button2
