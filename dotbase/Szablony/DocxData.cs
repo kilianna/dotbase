b24170130f206dfa045d45d6359372e11c211db7
kildom <kildom@users.noreply.github.com>
2024-10-20 13:48:40 +0200

    Jakieś zmiany w szablonach xml

--- dotbase/Szablony/DocxData.cs
+++ dotbase/Szablony/DocxData.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Globalization;
 
 namespace DotBase.Szablony
 {
+    class DocxDateTime
+    {
+        public Dictionary<string, string> tekst = new Dictionary<string, string>();
+        public int rok;
+        public int miesiac;
+        public int dzien;
+        public int godzina;
+        public int minuta;
+        public int sekunda;
+        public int milisekunda;
+
+        public DocxDateTime(DateTime dateTime)
+        {
+            tekst[Jezyk.EN.ToString()] = dateTime.ToString("dd MMMM yyyy", new CultureInfo("en-US"));
+            tekst[Jezyk.PL.ToString()] = dateTime.ToString("dd MMMM yyyy");
+            rok = dateTime.Year;
+            miesiac = dateTime.Month;
+            dzien = dateTime.Day;
+            godzina = dateTime.Hour;
+            minuta = dateTime.Minute;
+            sekunda = dateTime.Second;
+            milisekunda = dateTime.Millisecond;
+        }
+    }
+
     abstract class DocxData
     {
+        public Constants stale;
+
         protected abstract string FileName { get; }
+
+        public DocxData()
+        {
+            stale = Constants.getInstance();
+        }
 
         public void Generate(IWin32Window owner)
         {
