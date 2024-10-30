38d64afa08a0afb83a0fd0aa62a981d9a1d5d3f8
kildom <kildom@users.noreply.github.com>
2024-10-30 19:14:18 +0100

    Generacja dokumentu docx z szablonu

---
+++ dotbase/Szablony/DocxWindow.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace DotBase.Szablony
+{
+    partial class DocxWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(12, 9);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(148, 17);
+            this.infoLabel.TabIndex = 0;
+            this.infoLabel.Text = "Generacja dokumentu";
+            // 
+            // logBox
+            // 
+            this.logBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.logBox.BackColor = System.Drawing.SystemColors.Window;
+            this.logBox.Location = new System.Drawing.Point(12, 29);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logBox.Size = new System.Drawing.Size(836, 334);
+            this.logBox.TabIndex = 1;
+            // 
+            // DocxWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 375);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.infoLabel);
+            this.Name = "DocxWindow";
+            this.Text = "Generowanie dokumentu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.TextBox logBox;
+    }
+}
