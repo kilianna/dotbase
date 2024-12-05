45c246ee972f5ca1f14367c6f669c68851bfc878
kildom <kildom@users.noreply.github.com>
2024-12-05 10:40:25 -0800

    Przeniesinie na wersje .NET Framework v4.8

--- GeneratePackage.bat
+++ GeneratePackage.bat
@@ -43,6 +43,8 @@ call :check_vs %ProgramFiles%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Cu
 call :check_vs %ProgramFiles%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64
 call :check_vs %ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin
 call :check_vs %ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\amd64
+call :check_vs %ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin
+call :check_vs %ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64
 call :check_vs %ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin
 call :check_vs %ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64
 call :check_vs %ProgramFiles(x86)%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin
