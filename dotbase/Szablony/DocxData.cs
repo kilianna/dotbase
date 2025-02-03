b54245334bb1a209d0cc8ea8c1a182e2aa036660
kildom <kildom@users.noreply.github.com>
2025-02-04 00:23:29 +0100

    Początek świadectwa z nowego szablonu

--- dotbase/Szablony/DocxData.cs
+++ dotbase/Szablony/DocxData.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace DotBase.Szablony
 {
-    class DocxData
+    abstract class DocxData
     {
-        public void generate(IWin32Window owner, string outputFile)
+        protected abstract string FileName { get; }
+
+        public void Generate(IWin32Window owner)
         {
             Type thisType = this.GetType();
             var win = new DocxWindow();
+            var outputFile = Path.Combine(N.getProgramDir(), FileName);
             win.generate(owner, thisType.Name, this, outputFile);
             win.Dispose();
+        }
+
+        protected string DirGroup(string id, int count = 2)
+        {
+            if (id.Length > count)
+            {
+                return id.Substring(0, id.Length - count) + new string('x', count);
+            }
+            else
+            {
+                return new string('x', count);
+            }
         }
     }
 }
