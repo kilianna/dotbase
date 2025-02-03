783c356e6aca11e694aa029414e6a082a11d05ad
kildom <kildom@users.noreply.github.com>
2025-02-04 00:23:30 +0100

    Początek zminionego szablonu swiadecta na nowy sposób.

--- dotbase/Szablony/swiad_wzor.cs
+++ dotbase/Szablony/swiad_wzor.cs
@@ -1,25 +1,16 @@
 ﻿using System;
 using System.Data;
+using System.Windows.Forms;
 
 namespace DotBase.Szablony
 {
-
     class swiad_wzor : DocxData
     {
-        protected override string FileName
-        {
-            get { return String.Format(@"..\wyniki\Swiadectwo\{0}\{1}SwiadectwoWynik{2}.docx", DirGroup(nr_karty), nr_karty, JezykTools.kocowka(jezyk)); }
-        }
-
-        public Jezyk jezyk;
-        public string nr_karty;
-        public DateTime data_przyjecia;
-        public DateTime data_wykonania;
+        public int nr_karty;
         public DateTime data_wydania;
+        public DateTime data_wykonania;
+        public DateTime data_przyjecia;
         public string sprawdzil;
-        public DataRow zleceniodawca;
-        public DataRow przyrzad;
-        public DataRow jednostki;
         public bool poprawa;
         public string uwMD;
         public string uwD;
@@ -27,10 +18,25 @@ namespace DotBase.Szablony
         public string uwSMD;
         public string uwSD;
 
+        /*public DataRow zleceniodawca;
+        public DataRow przyrzad;
+        public DataRow jednostki;
         public DataRowCollection tabelaMD;
         public DataRowCollection tabelaD;
         public DataRowCollection tabelaSM;
         public DataRowCollection tabelaSD;
-        public DataRowCollection tabelaS;
+        public DataRowCollection tabelaS;*/
+
+        protected override string FileName
+        {
+            get {
+                return String.Format(@"..\wyniki\Swiadectwo\{0}\{1}SwiadectwoWynik{2}.docx", DirGroup(nr_karty), nr_karty, JezykTools.kocowka(jezyk));
+            }
+        }
+
+        protected override bool PreProcess(IWin32Window owner)
+        {
+            return true;
+        }
     }
 }
