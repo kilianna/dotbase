fd9ed21535074a0d4d0f6f929351018cbb225746
kildom <kildom@users.noreply.github.com>
2025-02-08 15:48:40 +0100

    Dodanie obsługi wyjątków w porównywaniu dokumentów oraz zwiększenie limitu czasu w testach

---
+++ docx-compare/exceptions.ts
@@ -0,0 +1,9 @@
+
+
+export const exceptions: { [key: string]: string | number } = {
+    '0,16=>0,15': 20304,
+    'mSv=>mGy': '20219,19348',
+    'przestrzenny równoważnik dawki=>kerma w powietrzu': '20219,19348',
+    'przestrzennego równoważnika dawki=>kermy w powietrzu': '20219,20064,19582,19497,19348',
+    '300 µSv/h.=>300&nbspµSv/h.': 20455,
+};
